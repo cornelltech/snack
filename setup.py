5648a1ba67a35cabd64809f1efd1b923af38e68e
Michael Wilber <wilmic1102@gmail.com>
2015-02-11 10:54:27 -0500

    No more march=native

--- setup.py
+++ setup.py
@@ -8,7 +8,12 @@ import numpy
 setup(name='snack',
       ext_modules = cythonize(Extension('_snack',["_snack.pyx"],
                                         include_dirs = [numpy.get_include()],
-                                        extra_compile_args = ['-fopenmp', '-O3', '-ffast-math', '-march=native'],
+                                        extra_compile_args = [
+                                            '-fopenmp',
+                                            '-O3',
+                                            '-ffast-math',
+                                            #'-march=native',
+                                            ],
                                         extra_link_args = ['-fopenmp'],
                   )),
       description="SNaCK embedding: Stochastic Neighbor and Crowd Kernel",
