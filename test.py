8cf385189f41b29d1c7010d74cdd8befa46d8a39
Michael Wilber <wilmic1102@gmail.com>
2015-02-20 17:15:13 -0500

    Add a testing script

--- test.py
+++ test.py
@@ -10,11 +10,15 @@ for i in xrange(10):
     params = X.ravel()
     D = euclidean_distances(X)
 
-    probs0 = t_sne._joint_probabilities(D, 30, False)
-    probs1 = snack.my_joint_probabilities(D, 30, False)
-    c1,grad1 = t_sne._kl_divergence(params, probs0, 1.0, len(X), 2)
+    probs1 = t_sne._joint_probabilities(D, 30, False)
+    probs2 = snack.my_joint_probabilities(D, 30, False)
+    c1,grad1 = t_sne._kl_divergence(params, probs1, 1.0, len(X), 2)
     c2,grad2 = snack.my_kl_divergence(params, probs1, 1.0, len(X), 2.0)
     print "Test", i
-    print "Difference norm:", np.linalg.norm(probs0 - probs1)
+    print "Difference norm:", np.linalg.norm(probs1 - probs2)
     print "Difference norm:", np.linalg.norm(grad1 - grad2)
     print "Difference norm:", c1-c2
+
+    assert np.allclose(probs1, probs2)
+    assert np.allclose(grad1, grad2)
+    assert np.allclose(c1, c2)
